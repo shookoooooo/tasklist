e542e4fb9bdd40afab5d285eabff22b95624f342
S <sho.ikma.0116@gmail.com>
2021-09-04 09:46:01 +0900

    NewServlet.javaを作成

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -8,7 +8,7 @@ public class DBUtil {
     private static final String PERSISTENCE_UNIT_NAME = "tasklist";
     private static EntityManagerFactory emf;
 
-    public static EntityManager createENtityManager() {
+    public static EntityManager createEntityManager() {
         return getEntityManagerFactory().createEntityManager();
     }
 
